75144dcdfdb9e2b8b81e3f4c22e6a08dcab0da97
NightKosh <nightkosh@gmail.com>
2016-06-20 23:04:24 +0300

    Fix

--- src/main/java/nightkosh/gravestone_extended/core/ModInfo.java
+++ src/main/java/nightkosh/gravestone_extended/core/ModInfo.java
@@ -8,9 +8,6 @@ package nightkosh.gravestone_extended.core;
  */
 public class ModInfo {
 
-    private ModInfo() {
-    }
-
     public static final String ID = "GraveStone-Extended";
     public static final String NAME = "GraveStone-Extended";
     public static final String VERSION = "1.0.0";
