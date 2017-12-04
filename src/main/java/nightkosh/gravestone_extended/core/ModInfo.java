47d0ddd674c61563a7ee78a99983b12a57935ecc
NightKosh <nightkosh@gmail.com>
2017-12-04 16:01:56 +0300

    Change mod version

--- src/main/java/nightkosh/gravestone_extended/core/ModInfo.java
+++ src/main/java/nightkosh/gravestone_extended/core/ModInfo.java
@@ -10,5 +10,5 @@ public class ModInfo {
 
     public static final String ID = "gravestone-extended";
     public static final String NAME = "GraveStone-Extended";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.3";
 }
