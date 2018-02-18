bf1036896ca80c432bfdb5dc6f9702bc3ef2953d
NightKosh <nightkosh@gmail.com>
2018-02-18 22:17:49 +0300

    Few fixes, #5

--- src/main/java/nightkosh/gravestone_extended/block/BlockCatacombsPortal.java
+++ src/main/java/nightkosh/gravestone_extended/block/BlockCatacombsPortal.java
@@ -3,10 +3,21 @@ package nightkosh.gravestone_extended.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockPortal;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import nightkosh.gravestone_extended.core.GSTabs;
 import nightkosh.gravestone_extended.core.ModInfo;
+
+import java.util.Random;
 
 /**
  * GraveStone mod
@@ -27,5 +38,49 @@ public class BlockCatacombsPortal extends BlockPortal {
 
     @Override
     public void neighborChanged(IBlockState state, World world, BlockPos pos, Block block, BlockPos fromPos) {
+    }
+
+    @Override
+    public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
+    }
+
+    @Override
+    public void onEntityCollidedWithBlock(World world, BlockPos pos, IBlockState state, Entity entity) {
+        if (!entity.isRiding() && !entity.isBeingRidden() && entity.isNonBoss()) {
+            entity.setPortal(pos);//TODO!!!
+        }
+    }
+
+    @Override
+    public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase entity, ItemStack stack) {
+        world.setBlockState(pos, state.withProperty(AXIS, EnumFacing.fromAngle(entity.rotationYaw).rotateY().getAxis()), 2);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void randomDisplayTick(IBlockState state, World world, BlockPos pos, Random rand) {
+        if (rand.nextInt(100) == 0) {
+            world.playSound(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, SoundEvents.BLOCK_PORTAL_AMBIENT, SoundCategory.BLOCKS, 0.5F, rand.nextFloat() * 0.4F + 0.8F, false);
+        }
+
+        for (int i = 0; i < 4; ++i) {
+            double x = pos.getX() + rand.nextFloat();
+            double y = pos.getY() + rand.nextFloat();
+            double z = pos.getZ() + rand.nextFloat();
+            double d3 = (rand.nextFloat() - 0.5) * 0.5;
+            double d4 = (rand.nextFloat() - 0.5) * 0.5;
+            double d5 = (rand.nextFloat() - 0.5) * 0.5;
+            int j = rand.nextInt(2) * 2 - 1;
+
+            if (world.getBlockState(pos.west()).getBlock() != this && world.getBlockState(pos.east()).getBlock() != this) {
+                x = pos.getX() + 0.5 + 0.25 * j;
+                d3 = rand.nextFloat() * 2 * j;
+            } else {
+                z = pos.getZ() + 0.5 + 0.25 * j;
+                d5 = rand.nextFloat() * 2 * j;
+            }
+
+            world.spawnParticle(EnumParticleTypes.PORTAL, x, y, z, d3, d4, d5);//TODO!!!!
+        }
     }
 }
